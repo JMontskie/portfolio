ee3d18e7c6de445c0b59850098b6e3d1ec59e33f
JMontskie <josephmanuelpmonton@gmail.com>
2022-08-20 10:06:42 +0800

    adding all files to repo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+   
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, shrink-to-fit=no">
+        
+        <title>Joseph Monton</title>
+
+        <script defer="defer" src="index.js"></script>
+        <link rel="stylesheet" href="main.bundle.css" type="text/css">
+    </head>
+    
+    <body>
+        <header id="header" class="fixed--bg" role="banner" style="transform: translate(0px,0%); opacity: 1;">
+            <div class="wrap-header--padding-sm">
+                <nav class="main-navigation">
+                    <div class="title">
+                        <a href="">
+                            <span class="h6"> Joseph Monton </span>
+                        </a>
+                        <span class="h6">UI/UX Designer, Developer</span>
+                    </div>
+                    <div id="main-menu" aria-expanded="false" role="navigation">
+                        <ul>
+                            <li class="h6">
+                                <a href="#cases">
+                                    works
+                                </a>
+                            </li> 
+                            <li class="h6">
+                                <a href="#intro" >
+                                    about
+                                </a>                           
+                            </li> 
+                            <li class="h6">
+                                <a href="#footer">
+                                    contact
+                                </a>                                    
+                            </li> 
+                        </ul>
+                    </div>
+                    <progress max="100" value="0"></progress>
+                </nav>        
+            </div>
+            <div class="scroller" style="opacity: 0;"></div>
+    </body>
+</html>
